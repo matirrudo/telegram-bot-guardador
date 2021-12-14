1d7bbe18e64b41eaee70dc5cb901794232bf4d72
matias <matirrudo@gmail.com>
2021-12-14 15:40:49 -0300

    Mostrar carpeta de documentos publicos

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const serveIndex = require('serve-index')
 require('./bot')
 const app = express()
 const PORT = process.env.PORT;
 app.set('port', PORT)
 
 app.use(express.static('public'))
-app.use('/resources', express.static(__dirname + '/public'))
-
+app.use('/resources/', express.static(__dirname + '/public'))
+app.use('/resources/documents', serveIndex(__dirname + '/public/documents'))
 app.listen(app.get('port'), () =>{
     console.log(`server on port ${PORT}`);
 })
